7d62b4b313e4ef6c0ed5a5f33859f4edcdd04d6d
tesla59 <nishant@heim.id>
2022-11-22 14:51:45 +0530

    commands: fully implement ls

--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 import * as shell from "./shell.js"
 import { readDir } from "./utils.js";
 import { exit } from 'node:process';
+import path from "node:path";
 
 export const ParseCommand = (command, args) => {
     switch (command) {
@@ -25,6 +26,20 @@ const pwd = () => {
 
 const ls = (args) => {
     if (!args.length) { readDir(currentDirectory).forEach((val) => { if (val[0] != '.') { console.log(val) } }); return }
+    for (let i = 0; i < args.length; i++) {
+        let dir
+        console.log("=>",args[i],":")
+        if (args[i][0] != '/') {
+            dir = path.join(currentDirectory, args[i])
+        } else {
+            dir = args[i]
+        }
+        try {
+            readDir(dir).forEach((val) => { if (val[0] != '.') { console.log(val) } })
+        } catch (err) {
+            console.log("Error: directory not found:", args[i])
+        }
+    }
 }
 
 const cd = (args) => {
