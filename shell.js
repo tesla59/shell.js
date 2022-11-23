13e525c3e813269e96f5693b952ffbf95634045b
tesla59 <nishant@heim.id>
2022-11-24 02:19:04 +0530

    early implementation of exec() to execute binaries

--- shell.js
+++ shell.js
@@ -1,6 +1,7 @@
 import promptSync from 'prompt-sync';
 import { userInfo } from "os";
 import { ParseCommand } from "./commands.js"
+import { exec } from "./utils.js"
 
 const prompt = promptSync({ sigint: false, eot: true, autocomplete: false });
 const BuiltInCommands = ["cd", "pwd", "ls", "fg", "exit"]
@@ -23,5 +24,7 @@ for (; ;) {
     // Built-in shell commands
     if (BuiltInCommands.includes(command)) {
         ParseCommand(command, args)
+        continue
     }
+    exec(command, args)
 }
--- utils.js
+++ utils.js
@@ -1,7 +1,13 @@
 import * as fs from 'fs';
+import { spawnSync } from 'child_process';
 
 export const readDir = (path) => {
     return fs.readdirSync(path, { withFileTypes: true })
         .filter((item) => item.isDirectory())
         .map((item) => item.name)
+}
+
+export const exec = (command, args) => {
+    var binary = spawnSync(command, args, { encoding: 'utf8', shell: true });
+    console.log(binary.stdout);
 }
